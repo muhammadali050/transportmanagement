a045b63302307a6ba6e0d2e380c4350a068fae90
Muhammad Ali <14051598-050@uog.edu.pk>
2018-03-20 00:00:57 +0500

    Attach AddMechanics activity. Attach AddDriver activity. Attach EditDriver activity. Attach EditMechanic activity. AddDriver design Change. Back button added in all ativities. Refract "Users" to "Drivers" in all class and xml file names. Create seprate profiles for Driver and Mechanic. Add working of Activity back button. Main Activity design added.

--- app/src/main/java/com/climesoftt/transportmanagement/RegistrationActivity.java
+++ app/src/main/java/com/climesoftt/transportmanagement/RegistrationActivity.java
@@ -1,10 +1,12 @@
 package com.climesoftt.transportmanagement;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by Ali on 3/14/2018.
@@ -33,5 +35,10 @@ public class RegistrationActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickRegister(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
