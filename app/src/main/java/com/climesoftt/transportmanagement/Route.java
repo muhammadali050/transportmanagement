3efb850c3f24b5bdf711119f67fdec5750be2d66
Muhammad Ali <14051598-050@uog.edu.pk>
2018-03-19 17:44:21 +0500

    Activity to view route added. Design changings.

---
+++ app/src/main/java/com/climesoftt/transportmanagement/Route.java
@@ -0,0 +1,19 @@
+package com.climesoftt.transportmanagement;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.climesoftt.transportmanagement.R;
+
+/**
+ * Created by Ali on 3/19/2018.
+ */
+
+public class Route extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+    }
+}
