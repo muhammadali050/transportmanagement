b840dbd9eed2fbf005592a88375ee76581ad3b1e
Muhammad Ali <14051598-050@uog.edu.pk>
2018-03-20 20:19:35 +0500

    Option for input driver on adding route has added.

---
+++ app/src/main/java/com/climesoftt/transportmanagement/DriverFaq.java
@@ -0,0 +1,13 @@
+package com.climesoftt.transportmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverFaq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_faq);
+    }
+}
--- app/src/main/java/com/climesoftt/transportmanagement/EditRoute.java
+++ app/src/main/java/com/climesoftt/transportmanagement/EditRoute.java
@@ -1,7 +1,9 @@
 package com.climesoftt.transportmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditRoute extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class EditRoute extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_route);
+    }
+
+    public void onClickEditRoute(View view) {
+        Intent intent = new Intent(this, EditRoute.class);
+        startActivity(intent);
     }
 }
