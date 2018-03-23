a9445651a8d96eb66a1ed744cea7f6d0a83b0037
Muhammad Ali <14051598-050@uog.edu.pk>
2018-03-23 15:35:05 +0500

    All Faq activity added with ready UI and Adapter. Menue added to All Faq activity. Add Question Activity added. AdminDashboard activity attached. Edit Question Option added. Edit question activity added. Delete question option added. Admin Dashboard design updated. Driver dashboard design updated

--- app/src/main/java/com/climesoftt/transportmanagement/MainActivity.java
+++ app/src/main/java/com/climesoftt/transportmanagement/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity{
 
         }
 
-        Intent intent = new Intent(this, DriverFaq.class);
+        Intent intent = new Intent(this, AdminDashboardActivity.class);
         startActivity(intent);
 
     }
